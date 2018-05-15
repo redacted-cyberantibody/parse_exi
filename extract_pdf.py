f83a6a437aa0b808675ee5edf89c9ff7f3da8710
ckswilliams <chris.williams@health.qld.gov.au>
2018-05-15 11:01:14 +1000

    Improve speed and usability of extract_pdf

--- extract_pdf.py
+++ extract_pdf.py
@@ -6,14 +6,25 @@ Created on Tue May  1 11:05:24 2018
 """
 
 from tkinter import filedialog
+from tkinter import simpledialog
 from pdf2image import convert_from_path
+import os
 
+page_start = simpledialog.askinteger('First extraction page','Choose a starting page for image extraction')
+page_end = simpledialog.askinteger('Last extraction page', 'Choose and ending page for image extraction')
 fn = filedialog.askopenfilename()
 fn_split = fn.split('/')
 name = fn_split[-1]
 folder = '/'.join(fn_split[0:-1]) + '/'
+output_folder = folder + 'pdf2image_output'
+try:
+    os.mkdir(output_folder)
+except:
+    pass
+output_folder = output_folder + '/'
 
-pages = convert_from_path(fn)
+pages = convert_from_path(fn, first_page=page_start, last_page=page_end)
 
 for i, page in enumerate(pages):
-    page.save(folder + name.split('.')[0]+str(i+1)+'.tif')
+    print(str(i) + ' out of ' + len(pages))
+    page.save(folder + name.split('.')[0]+str(i+1)+'.png')
